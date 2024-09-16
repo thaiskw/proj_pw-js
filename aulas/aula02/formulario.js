b8a5c15b9771cec3e1bce85b095ed7ede8b69cdb
Thainá Rodrigues de Lima <thaina.lima47@etec.sp.gov.br>
2024-09-16 18:33:19 -0300

    correção console js

--- aulas/aula02/formulario.js
+++ aulas/aula02/formulario.js
@@ -8,22 +8,22 @@ function adicionarLinha(){
     const demissao = document.getElementById("demissao").value;
 
     //Se for preenchido vazio
-    if(nome === '' || idade === '' || admissao === '' || demissao === ''){
+    if(nome === '' || idade === '' || admissao === ''){
         alert("Preencha os valores do formulário!")
-    }
-
+    } else{
+        
     //Cria uma linha da tabela se não existir
     const tabela = document.getElementById("tabelaDados").getElementsByTagName('tbody') [0];
 
     //Inserindo uma nova linha
-    const novaLinha = tabela.insertRow;
+    const novaLinha = tabela.insertRow();
 
     //Inserindo os valores da linha
-    const celId = novaLinha.InsertCel(0);
-    const celNome = novaLinha.InsertCel(1);
-    const celIdade = novaLinha.InsertCel(2);
-    const celAdmissao = novaLinha.InsertCel(3);
-    const celDemissao = novaLinha.InsertCel(4);
+    const celId = novaLinha.insertCell(0);
+    const celNome = novaLinha.insertCell(1);
+    const celIdade = novaLinha.insertCell(2);
+    const celAdmissao = novaLinha.insertCell(3);
+    const celDemissao = novaLinha.insertCell(4);
 
     //Inserindo os valores dentro das celulas
     celId.innerHTML = proximoId;
@@ -36,4 +36,6 @@ function adicionarLinha(){
 
     //Limpar Formulário
     document.getElementById("linhaForm").reset();
+    }
+
 }
